9c01271de551f054fb4f538a699d7eb1fec5f6de
roma_marshall <roma_marshall@outlook.com>
2023-06-19 20:04:30 +0200

    add head

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="my-20 mx-7">
-    <div class="font-['Rock_3D'] uppercase select-none">
-      <p class="text-3xl ml-6">henri</p>
-      <h1 class="text-hero -mt-16 -mb-8">matisse</h1>
+  <div class="my-7 mx-0 max-w-fit">
+    <div class="flex justify-end text-lg mb-60">
+        <div class="mx-20">
+          <a href="#">biographie</a>
+        </div>
+        <div class="space-x-6">
+          <a href="#">artistique</a>
+          <a href="#">les expositions</a>
+        </div>
+        <div class="mx-20">
+          <a href="#">médias</a>
+        </div>
+    </div>
+    <div class="font-['Rock_3D'] uppercase select-none ">
+      <p class="text-4xl ml-6 tracking-widest">henri</p>
+      <h1 class="text-hero -mt-20 -mb-10">matisse</h1>
     </div>
     <div class="text-lg ml-5">
       <p>artiste peintre français,</p>
