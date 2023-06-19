6a8c89037445f15a3edc27f71dab1b93e36b4b05
roma_marshall <roma_marshall@outlook.com>
2023-06-19 20:09:20 +0200

    edit spacing

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="my-7 mx-0 max-w-fit">
     <div class="flex justify-end text-lg select-none mb-60">
-        <div class="mx-20">
+        <div class="mr-24">
           <a href="#">biographie</a>
         </div>
         <div class="space-x-6">
           <a href="#">artistique</a>
           <a href="#">les expositions</a>
         </div>
-        <div class="mx-20">
+        <div class="ml-24">
           <a href="#">médias</a>
         </div>
     </div>
     <div class="font-['Rock_3D'] uppercase select-none">
       <p class="text-4xl ml-6 tracking-widest">henri</p>
-      <h1 class="text-hero -mt-20 -mb-10">matisse</h1>
+      <h1 class="text-hero -mt-20 -mb-10 tracking-tighter">matisse</h1>
     </div>
     <div class="text-lg ml-5">
       <p>artiste peintre français,</p>
